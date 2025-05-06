82fe3ee7fccb253a82d3610ada0965318a1e0ed9
JoseAngelMarquez <126813977+JoseAngelMarquez@users.noreply.github.com>
2025-05-07 01:12:02 +0200

    Update example.java

--- example.java
+++ example.java
@@ -1,20 +1,5 @@
-import java.io.File;
-
-public class ListarDirectorios {
+public class HolaMundo {
     public static void main(String[] args) {
-        String ruta = System.getProperty("user.dir");
-        System.out.println("Ruta actual: " + ruta);
-
-        File carpeta = new File(ruta);
-        String[] archivos = carpeta.list();
-
-        if (archivos != null) {
-            System.out.println("Contenido del directorio:");
-            for (String archivo : archivos) {
-                System.out.println(" - " + archivo);
-            }
-        } else {
-            System.out.println("No se pudo acceder al directorio.");
-        }
+        System.out.println("Hola, mundo");
     }
 }
